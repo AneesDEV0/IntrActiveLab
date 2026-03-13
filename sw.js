1f01d8fcc975d02ff9429c9564fe361ebfc1423c
AneesDEV0 <aneesrafat0@gmail.com>
2026-03-13 12:31:24 +0200

    add PWA

--- sw.js
+++ sw.js
@@ -4,13 +4,30 @@ self.addEventListener('install', (e) => {
       './',
       './index.html',
       './css/style.css',
-      './js/script.js'
+      './js/script.js',
+      './manifest.json',
+      './video/icon.jpeg',
+      './pages/ThreeD.html',
+      './pages/fungus.html',
+      './pages/lifecycle.html',
+      './pages/name.html',
+      './css/lifecycle.css',
+      './css/fungus.css',
+      './css/name.css',
+      './css/ThreeD.css'
     ]))
   );
 });
 
 self.addEventListener('fetch', (e) => {
   e.respondWith(
-    caches.match(e.request).then((response) => response || fetch(e.request))
+    caches.match(e.request).then((response) => {
+      return response || fetch(e.request).then((fetchResponse) => {
+        return caches.open('lab-store').then((cache) => {
+          cache.put(e.request, fetchResponse.clone());
+          return fetchResponse;
+        });
+      });
+    })
   );
 });
